cf803907d367488563d8e427a0f49548dfcf5033
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-04-25 17:39:15 +0300

    Fix: keep dimention while taking mask to avoid dimention errors.

--- loss.py
+++ loss.py
@@ -10,8 +10,8 @@ class YOLOLoss(nn.Module):
         self.class_loss = nn.BCEWithLogitsLoss(pos_weight=torch.tensor(positive_coef))
 
     def forward(self, input, target):
-        obj_mask = target[:, 0]
+        obj_mask = target[:, :1]
         spatial_loss = F.mse_loss(input[:, 1:] * obj_mask, target[:, 1:] * obj_mask)
-        class_loss = self.class_loss(input[:, 0], obj_mask)
+        class_loss = self.class_loss(input[:, :1], obj_mask)
         return self.spatial_coef * spatial_loss + class_loss
 
