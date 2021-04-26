a1213664066130cbf4b56f2be2024bba3052088b
Sokol Dmitrii <sokol.dmitry.f@gmail.com>
2021-04-26 03:23:36 +0300

    Fixed errors in transforming dataset.

--- preprocess_dataset.sh
+++ preprocess_dataset.sh
@@ -3,12 +3,14 @@ mkdir -p checkpoints/day2nightA
 cp extra/Day2Night_model/checkpoints/daynight_cyclegan_new/latest_net_D_A.pth checkpoints/day2nightA/latest_net_D.pth
 cp extra/Day2Night_model/checkpoints/daynight_cyclegan_new/latest_net_G_A.pth checkpoints/day2nightA/latest_net_G.pth
 
-!mkdir dataroot -p
-!unzip extra/dhd.zip -d dataroot
-!rm dataroot/dhd_traffic/images/train/1502433988638.jpg
-!rm dataroot/dhd_traffic/images/train/1502445483179.jpg
+mkdir dataroot -p
+unzip extra/dhd.zip -d dataroot
+rm dataroot/dhd_traffic/images/train/1502433988638.jpg
+rm dataroot/dhd_traffic/images/train/1502445483179.jpg
 
 for subset in 'train' 'val'; do
+    rm -rf results/
+
     python3 pytorch-CycleGAN-and-pix2pix/test.py --eval \
         --phase test \
         --name day2nightA \
